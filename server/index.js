38ea5a06073cc96ca43e9dc576bbdfa64303e32e
Rich <web_dev2017@outlook.com>
2022-07-24 16:41:29 +0100

    Mongoose Setup

--- server/index.js
+++ server/index.js
@@ -3,9 +3,18 @@ const express = require('express');
 require('dotenv').config();
 
 const allRoutes = require('./routes/router');
+const connectDB = require('./config/db');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//Connect to database
+connectDB().then(() => {
+    app.listen(port, console.log(`Server running on port ${port}`));
+}).catch((error) => {
+    console.log(error);
+});
 
 const subDirectory = '/api/items';
 
@@ -17,5 +26,3 @@ app.use((req, res, next) => {
 });
 
 app.use(subDirectory, allRoutes);
-
-app.listen(port, console.log(`Server running on port ${port}`));
