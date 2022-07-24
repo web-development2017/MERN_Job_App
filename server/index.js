0fe08e524a9c3264d253a83c1d5c42abbe9d919e
Rich <web_dev2017@outlook.com>
2022-07-24 19:42:45 +0100

    Host on Heroku

--- server/index.js
+++ server/index.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 require('dotenv').config();
 
+const cors = require('cors');
+
 const allRoutes = require('./routes/allRoutes');
 const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+
+if(process.env.NODE_ENV === "production"){
+    console.log("Production");
+   
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
+    });
+    
+}else{
+    console.log("Development");
+}
 
 //Middleware
 app.use(express.json());
