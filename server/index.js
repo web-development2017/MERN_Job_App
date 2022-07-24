434b24cbe9399ad0ac3f4bc641e63185595b537d
Rich <web_dev2017@outlook.com>
2022-07-24 16:35:26 +0100

    Middleware

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 require('dotenv').config();
 
-console.log("Success reading from .env file " + process.env.PORT)
-
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//Global middleware that run on every time between req res
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+});
+
+//routes middleware 
+app.get('/', (req, res) => {
+    res.json({ message: 'MERN_App' })
+});
 
 app.listen(port, console.log(`Server running on port ${port}`));
